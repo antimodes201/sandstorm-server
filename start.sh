2e69cb27a8255700b4c67df9f25b32ce65cfb40b
antimodes201 <bob.krause201@gmail.com>
2019-11-12 15:00:29 -0500

    fix spacing for real

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Start script for Sandstorm Dedicated Server Docker Image
 
 /steamcmd/steamcmd.sh +login anonymous +force_install_dir /sandstorm +app_update 581330 +quit
-/sandstorm/Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping -hostname="${INSTANCE_NAME}" -Port=${GAME_PORT} -QueryPort=${QUERY_PORT} 
+/sandstorm/Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping -hostname='${INSTANCE_NAME}' -Port=${GAME_PORT} -QueryPort=${QUERY_PORT} 
